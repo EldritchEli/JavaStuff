f0ce990f74d8249de10769b141ed0021266a8afc
wushmachine <Eli.uhlin@gmail.com>
2022-01-17 13:52:24 +0100

    bipboop

---
+++ src/Fishfoosh.java
@@ -0,0 +1,12 @@
+public class Fishfoosh {
+
+    int fish = 0;
+    String foosh = "foosh";
+    public Fishfoosh() {
+    }
+
+    public Fishfoosh(int fish, String foosh) {
+        this.fish = fish;
+        this.foosh = foosh;
+    }
+}
